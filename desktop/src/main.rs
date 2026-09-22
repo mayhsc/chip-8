0a85f61359b9f354bf269bf3415fa6da4e31534e
unpublished3 <chapagainsakshyam@gmail.com>
2026-09-22 15:39:43 +0545

    Draw on the screen

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,6 +1,11 @@
 use chip8_core::*;
-use sdl3::{event::Event, pixels::Color};
+use sdl3::rect::Rect;
+use sdl3::{VideoSubsystem, event::Event, pixels::Color, render::Canvas, video::Window};
 use std::env;
+use std::fs::File;
+use std::io::Read;
+
+const SCALE: u32 = 15;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -9,22 +14,17 @@ fn main() {
         return;
     }
 
-    const SCALE: u32 = 15;
-    const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
-    const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
-
     let sdl_context = sdl3::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem
-        .window("Chip-8 Emulator", WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas();
     let mut event_pump = sdl_context.event_pump().unwrap();
     let color_offset = 0;
+
+    let mut canvas = create_canvas(&video_subsystem);
+    let mut chip8 = Emu::new();
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer = Vec::new();
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
 
     'gameloop: loop {
         canvas.set_draw_color(Color::RGB(color_offset, 64, 255 - color_offset));
@@ -38,7 +38,41 @@ fn main() {
                 _ => {}
             }
         }
+        chip8.tick();
+        draw_screen(&chip8, &mut canvas);
     }
 
     canvas.present();
+}
+
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::BLACK);
+    canvas.clear();
+    let screen_buf = emu.get_display();
+    canvas.set_draw_color(Color::WHITE);
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+            // let rect = Rect::new((x  SCALE) as i32, (y  SCALE) as i32, SCALE, SCALE);
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+}
+
+fn create_canvas(video_subsystem: &VideoSubsystem) -> Canvas<Window> {
+    const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+    const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
+    let window = video_subsystem
+        .window("Chip-8 Emulator", WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let canvas = window.into_canvas();
+
+    canvas
 }
