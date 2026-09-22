1953394f3cedb44f4392d3d2fc3c9e3f34dfd3aa
unpublished3 <chapagainsakshyam@gmail.com>
2026-09-22 17:04:17 +0545

    Process keypresses

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,4 +1,5 @@
 use chip8_core::*;
+use sdl3::keyboard::Keycode;
 use sdl3::rect::Rect;
 use sdl3::{VideoSubsystem, event::Event, pixels::Color, render::Canvas, video::Window};
 use std::env;
@@ -33,8 +34,26 @@ fn main() {
 
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'gameloop;
+                }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                }
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
                 }
                 _ => {}
             }
@@ -80,4 +99,26 @@ fn create_canvas(video_subsystem: &VideoSubsystem) -> Canvas<Window> {
     let canvas = window.into_canvas();
 
     canvas
+}
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::_1 => Some(0x1),
+        Keycode::_2 => Some(0x2),
+        Keycode::_3 => Some(0x3),
+        Keycode::_4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
 }
